ade3807cc537582adebf8d53848e86859f2e05d6
Rodrigo Gaspar <rod.gaspar@gmail.com>
2023-01-22 12:11:31 -0300

    Adding basics operations program

---
+++ basics/basics.py
@@ -0,0 +1,30 @@
+# This is my second python program
+
+# Operators
+num = 10 + 20
+print("The result is: " , num)
+
+num = 10 - 20
+print("The result is: " , num)
+
+num = 10 * 20
+print("The result is: " , num)
+
+num = 10 / 5 
+print("The result is: " , num)
+
+cond = False
+print(cond)
+
+if cond:
+    print("This condition is true")
+else:
+    print("This condition is false")
+    
+numbers = { 10, 20, 30, 40, 50, 60}
+for number in numbers:
+    if number == 10:
+        print("Number 10")
+    if number == 60:
+        print("Number 60")
+    
